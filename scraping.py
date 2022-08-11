ec7357a925dd126bbaa992baea0daaaa3da09b5d
clarkb21 <bmclark7694@gmail.com>
2022-08-11 12:29:56 -0400

    Adding challenge files

--- scraping.py
+++ scraping.py
@@ -20,7 +20,8 @@ def scrape_all():
       "news_paragraph": news_paragraph,
       "featured_image": featured_image(browser),
       "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
+      "last_modified": dt.datetime.now(),
+      "hemisphere_data": hemisphere_data(browser)
     }
     #Stop the webdriver and return data
     browser.quit()
@@ -91,6 +92,36 @@ def mars_facts():
     df.set_index('Description', inplace=True)
     
     return df.to_html()
+
+# ------ Hemisphere Title and Image ----- 
+
+def hemisphere_data():
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for i in range(4):
+        hemispheres = {}
+        
+        browser.find_by_css('a.product-item h3')[i].click()
+        element = browser.links.find_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css("h2.title").text
+        hemispheres["img_url"] = img_url
+        hemispheres["title"] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+    # 4. Return the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
+   # 5. Quit the browser
+    browser.quit()
+
 
 if __name__ == "__main__":
     # If running as script, print scraped data
